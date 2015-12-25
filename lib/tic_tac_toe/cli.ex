ae0b108717577bbf9965deb4c77596b90a721029
Reid Paape <wrpaape@gmail.com>
2015-12-25 09:05:11 -0600

    cli and helper :

--- lib/tic_tac_toe/cli.ex
+++ lib/tic_tac_toe/cli.ex
@@ -13,19 +13,16 @@ defmodule TicTacToe.CLI do
     |> process
   end
 
-  def process(:help) do
-    "failed to parse board_size (integer)"
-    |> Helper.cap(ANSI.red, ANSI.reset)
+  #external API ^
+
+  def process({board_size, _}) when board_size > 0 and board_size < 5 do
+    board_size
+    |> TicTacToe.start_link
   end
-  
-  def process(:error) do
-    "failed to parse board_size (integer)"
-    |> Helper.cap(ANSI.red, ANSI.reset)
-  end
-   
-  def process({board_size, _}) do
-    
-  end
+
+  def process(:help),           do: alert_and_halt("usage: tic_tac_toe <board_size>", ANSI.blue)
+  def process(:error),          do: alert_and_halt("failed to parse board_size (integer)")
+  def process({board_size, _}), do: alert_and_halt("board_size must be an integer > 0 and < 5")
 
   def parse_args(argv) do
     argv
@@ -37,5 +34,14 @@ defmodule TicTacToe.CLI do
  
        {_, [board_size_str], _}  -> Integer.parse(board_size_str)
     end
+  end
+
+  #helpers V
+
+  defp alert_and_halt(msg, color \\ ANSI.red) do
+    msg   
+    |> Helper.cap(color, ANSI.reset)
+
+    System.halt(0)
   end
 end
