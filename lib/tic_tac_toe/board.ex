99b0d3fa3289b4785ec8a66f18dd3a1901df1dc9
Reid Paape <wrpaape@gmail.com>
2015-12-26 15:08:59 -0600

    winning moves calculator working

--- lib/tic_tac_toe/board.ex
+++ lib/tic_tac_toe/board.ex
@@ -11,7 +11,7 @@ defmodule TicTacToe.Board do
 
   # external API ^
   
-  def init(board_size \\ 3) do
+  def init(board_size) do
     open_moves =
       board_size
       |> :math.pow(2)
@@ -22,8 +22,9 @@ defmodule TicTacToe.Board do
       end)
 
     winning_moves =
-      board_size
-      |> calculate_winning_moves(open_moves)
+      open_moves
+      |> Enum.chunk(board_size)
+      |> calculate_winning_moves
 
     board =
       open_moves
@@ -48,19 +49,42 @@ defmodule TicTacToe.Board do
 
   # helpers v
 
-  def calculate_winning_moves(moves, board_size) do
-    moves
-    |> Enum.slice(board_size - 1..-board_size)
-    |> Helper.wrap_app(board_size - 1)
-    |> List.wrap
-    |> List.insert_at(0, ({moves, board_size + 1})
-    |> Enum.map(fn({diag_range, nth})->
-      diag_range
-      |> Enum.take_every(nth)
-      |> Enum.into(HashSet.new)
-    end)
-    |> Enum.into(HashSet.new)
+  def calculate_winning_moves(row_chunks) do
+    rows =
+      row_chunks
+      |> Enum.reduce(HashSet.new, fn(chunk, rows)->
+        rows
+        |> Set.put(Enum.into(chunk, HashSet.new))
+      end)
+      
+    rows_cols =
+      row_chunks
+      |> List.zip
+      |> Enum.reduce(rows, fn(chunk_tup, rows_cols)->
+        next_cols =
+          chunk_tup 
+          |> Tuple.to_list
+          |> IO.inspect
+          |> Enum.into(HashSet.new)
 
+        rows_cols
+        |> Set.put(next_cols)
+      end)
+
+    row_chunks
+    |> Enum.reduce([{HashSet.new, 0, 1}, {HashSet.new, -1, -1}], fn(chunk, diag_tups)->
+      diag_tups
+      |> Enum.map(fn({diag, at, inc})->
+        diag
+        |> Set.put(Enum.at(chunk, at)) 
+        |> Helper.wrap_app(at + inc)
+        |> Tuple.append(inc)
+      end)
+    end)
+    |> Enum.reduce(rows_cols, fn({diag, _, _}, winning_moves)->
+      winning_moves
+      |> Set.put(diag)
+    end)
   end
 end
 
