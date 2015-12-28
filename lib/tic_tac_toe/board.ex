8f692297673e309c89fe6e5d05078094b9bdf388
Reid Paape <wrpaape@gmail.com>
2015-12-27 19:03:36 -0600

    moving on to printer

--- lib/tic_tac_toe/board.ex
+++ lib/tic_tac_toe/board.ex
@@ -18,7 +18,7 @@ defmodule TicTacToe.Board do
 
   def state,                 do: GenServer.call(__MODULE__, :state)
 
-  def next_win_state(move, token, win_state), do: next_win_info(move, token, win_state, [])
+  def next_win_state(move, token, win_state), do: next_info(move, token, win_state, [])
 
   # external API ^
   
@@ -56,48 +56,44 @@ defmodule TicTacToe.Board do
 
   # helpers v
   
-  defmacrop recurse(next_win_info) do
+  defmacrop recurse(next_acc_state) do
     quote do
-      var!(acc_win_state) = [unquote(next_win_info) | var!(acc_win_state)]
-
-      recurse
+      next_info(var!(move), var!(token), var!(rem_state), unquote(next_acc_state))
     end
   end
 
-  defmacrop recurse do
-    quote do
-      next_win_info(var!(move), var!(token), var!(rem_win_state), var!(acc_win_state))
-    end
+  defmacrop push_next(next_info) do
+    quote do: recurse([unquote(next_info) | var!(acc_state)])
   end
 
-  defmacrop reduce_owned_or_unclaimed_win_info_and_recurse do
+  defmacrop reduce_owned_or_unclaimed_info_and_recurse do
     quote do
       var!(win_set)
       |> Set.delete(var!(move))
       |> case do
-        %HashSet{size: ^var!(size)} -> recurse(var!(win_info))
+        %HashSet{size: ^var!(size)} -> push_next(var!(info))
         %HashSet{size: 0}           -> :win
-        next_win_set                -> recurse({next_win_set, var!(token)})
+        next_win_set                -> push_next({next_win_set, var!(token)})
       end
     end
   end
 
-  def next_win_info(move, token, [win_info = {win_set = %HashSet{size: size}, token} | rem_win_state], acc_win_state) do
-    reduce_owned_or_unclaimed_win_info_and_recurse
+  def next_info(move, token, [info = {win_set = %HashSet{size: size}, token} | rem_state], acc_state) do
+    reduce_owned_or_unclaimed_info_and_recurse
   end
 
-  def next_win_info(move, token, [win_info = win_set = %HashSet{size: size} | rem_win_state], acc_win_state) do
-    reduce_owned_or_unclaimed_win_info_and_recurse
+  def next_info(move, token, [info = win_set = %HashSet{size: size} | rem_state], acc_state) do
+    reduce_owned_or_unclaimed_info_and_recurse
   end
 
-  def next_win_info(move, token, [occ_win_info | rem_win_state], acc_win_state) do
-    occ_win_info
+  def next_info(move, token, [occ_info | rem_state], acc_state) do
+    occ_info
     |> elem(0)
     |> Set.member?(move)
-    |> if do: recurse, else: recurse(occ_win_info)
+    |> if do: recurse(acc_state), else: push_next(occ_info)
   end
 
-  def next_win_info(_move, _token, [], []),             do: :tie
-  def next_win_info(_move, _token, [], next_win_state), do: next_win_state
+  def next_info(_move, _token, [], []),             do: :tie
+  def next_info(_move, _token, [], next_win_state), do: next_win_state
 end
 
