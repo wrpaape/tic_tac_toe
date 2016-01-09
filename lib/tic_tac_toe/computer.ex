a7be51ed3ba20c247782186f5060b5b85d24ea09
Reid Paape <wrpaape@gmail.com>
2016-01-09 15:58:06 -0600

    must finish

--- lib/tic_tac_toe/computer.ex
+++ lib/tic_tac_toe/computer.ex
@@ -21,119 +21,124 @@ defmodule TicTacToe.Computer do
 
   def handle_call(:state, _from, state), do: {:ok, state, state}
 
-  def handle_call({:next_move, {board, valids, win_state}}, _from, chars = %{1 => my_char}) do
+  def handle_call({:next_move, {board, valids, win_state}}, _from, chars) do
     board
     |> IO.write
 
     collector_pid =
       __MODULE__
-      |> spawn(:collect, ["", length(valids), self])
+      |> spawn(:collect, [nil, length(valids), HashSet.new, self])
     
-
-    valids
-    |> Enum.reduce_while({[], valids}, fn(_move, {before_move, [move | after_move]})->
-      move
-      |> EndGame.next_win_state(my_char, win_state) 
-      |> case do
-        1 ->
-          self
-          |> send(move)
-
-          {:halt, :guaranteed_win}
+    branch_workers =
+      valids
+      |> Enum.reduce({HashSet.new, [], valids}, fn(_move, {branch_workers, before_move, [move | after_move]})->
+        worker_pid =
+          __MODULE__
+          |> spawn(:sum_branch, [win_state, chars, before_move, after_move, move, collector_pid])
+        {Set.put(branch_workers, worker_pid), [move | before_move], after_move}
+      end)
+      |> elem(0)
 
 
-        next_win_state ->
-          next_win_state
-          |> case do
-            0 ->
-              collector_pid
-              |> send({0, move})
 
-            _ ->
-              __MODULE__
-              |> spawn(:sum_branch, [before_move ++ after_move,
-                                     chars,
-                                     next_win_state,
-                                     collector_pid,
-                                     move])
-          end
 
-          {:cont, {[move | before_move], after_move}}
-      end
-    end)
 
     receive do
-      best_move ->
+      {dead_workers, best_move} ->
+        branch_workers
+        |> Set.difference(dead_workers)
+        |> Enum.each(&Process.exit(&1, :kill))
+
         best_move
         |> IO.write
-
-        collector_pid
-        |> Process.exit(:kill)
 
       {:reply, best_move, chars}
     end
   end
 
-  def do_branch(done, mult, _, _, _) 
-  def do_branch(done, mult, _, _, _) when is_number(done),       do: done * mult
-  def do_branch(win_state, mult, chars, before_move, after_move) do
-    before_move
-    |> Enum.concat(after_move)
-    |> reduce_branch(mult * -1, chars, win_state)
-  end
-
-  def reduce_branch(rem_moves, mult, chars, win_state) do
-    next_char =
+  # def do_branch(done, mult, chars, _, _) when is_number(done) do
+    # IO.inspect("node score: #{done}, mult: #{mult}, char: #{chars[mult]}")
+    # done * mult
+  # end
+  def do_branch(rem_moves, mult, chars, win_state) do
+    char =
       chars
       |> Map.get(mult)
 
-    {next_acc_sum, next_win_states, _, _} =
-      rem_moves
-      |> Enum.reduce({0, [], [], rem_moves}, fn(_move, {acc_sum, win_states, before_move, [move | after_move]})->
-        {next_acc_sum, next_win_states} =
-          move
-          |> EndGame.next_win_state(next_char, win_state)
-          |> case do
-            done when is_number(done) ->
-              {acc_sum + done, win_states}
+    rem_moves
+    |> Enum.reduce_while({[], [], rem_moves}, fn(_move, {next_branches, before_move, [move | after_move]})->
+        move
+        |> EndGame.next_win_state(char, win_state)
+        |> case do
+          done when is_number(done) ->
+            IO.puts("STOOP done: #{done * mult} char: #{char} move: #{move} bef: #{before_move} aft: #{after_move}")
+            {:halt, done}
 
-            win_state ->
-              {acc_sum, [{win_state, before_move ++ after_move} | win_states]}
-          end
+          next_win_state ->
 
-        {next_acc_sum, next_win_states, [move | before_move], after_move}
-      end)
-
-    next_win_states
-    |> Enum.reduce(next_acc_sum * mult, fn({win_state, rem_moves}, branch_max)->
-      rem_moves
-      |> reduce_branch(-mult, chars, win_state)
-      |> max(branch_max)
+            {:cont, {[{before_move, after_move, next_win_state}], [move | before_move], after_move}}
+        end
     end)
+    |> case do
+      {next_branches, _, _} ->
+        next_mult = mult * -1
+
+        next_branches
+        |> Enum.reduce(-2, fn({before_move, after_move, next_win_state}, last_score)->
+          branch_score =
+            before_move
+            |> Enum.concat(after_move)
+            |> do_branch(next_mult, chars, next_win_state)
+
+          last_score
+          |> max(branch_score)
+        end)
+
+      done ->
+        done * mult
+    end
   end
 
+  def sum_branch(win_state, chars = %{1 => my_char}, before_move, after_move, move, collector_pid) do
+    msg =
+      move
+      |> EndGame.next_win_state(my_char, win_state)
+      |> case do
+        done when is_number(done) ->
+          :game_over
 
-  def sum_branch(rem_moves, chars, win_state, collector_pid, move) do
-    branch_max =
-      rem_moves
-      |> reduce_branch(-1, chars, win_state)
+        next_win_state ->
+          before_move
+          |> Enum.concat(after_move)
+          |> do_branch(-1, chars, next_win_state)
+      end
 
     collector_pid
-    |> send({branch_max, move})
+    |> send({msg, move, self})
 
     exit(:kill)
   end
 
   # helpers v
 
-  def collect({_, best_move}, 0, parent_pid),  do: send(parent_pid, best_move)
-  def collect(last_max, rem_moves, parent_pid) do
+  def collect({_, best_move}, 0, dead_workers, root_pid) do
+    root_pid 
+    |> send({dead_workers, best_move})
+
+    exit(:kill)
+  end
+
+  def collect(last_max, rem_moves, dead_workers, root_pid) do
     receive do
-      move_sum ->
-        move_sum 
+      {:game_over, best_move, worker_pid} ->
+        root_pid
+        |> send({Set.put(dead_workers, worker_pid), best_move})
+
+      {score, move, worker_pid} ->
+        {score, move}
         |> IO.inspect
-        |> min(last_max)
-        |> collect(rem_moves - 1, parent_pid)
+        |> max(last_max)
+        |> collect(rem_moves - 1, Set.put(dead_workers, worker_pid), root_pid)
     end
   end
 end
